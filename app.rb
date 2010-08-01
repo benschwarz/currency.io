85609971b1ac56dccdda21ae9125e525423a1b4d
Ben Schwarz <ben.schwarz@gmail.com>
2010-08-01 18:21:35 +1000

    Added cache manifest

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'rest-client'
 
 Sass::Plugin.options[:syntax] = :scss
 
+mime_type :manifest, "text/cache-manifest"
+
 get '/' do
   haml :index
 end
@@ -9,4 +11,9 @@ end
 get '/exchange' do
   content_type :json
   RestClient.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'http%3A%2F%2Fwww.google.com%2Fig%2Fcalculator%3Fq%3D#{params[:from]}%2520#{params[:to]}'&format=json").to_s  
+end
+
+get '/offline.manifest' do
+  content_type :manifest
+  erb "offline.manifest"
 end
