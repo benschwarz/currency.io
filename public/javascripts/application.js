b109d43f51055299ca7c73a0fcd286598b81a81e
Anthony Kolber <ae@aestheticallyloyal.com>
2010-08-19 09:17:58 +1000

    Moved dataset into its own js file

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,26 +8,6 @@
 
 */
 
-window.currencies = {
-  "AUD": {
-    "name": "Australian Dollars",
-    "symbol": "$",
-    "rate_usd": "0.904615"
-  },
-  "JPY": {
-    "name": "Japanese Yen",
-    "symbol": "¥",
-    "rate_usd": "0.0117311"
-  },
-  "CHF": {
-    "name": "Swiss Francs",
-    "symbol": "&#8366;",
-    "rate_usd": "0.961658"
-  }
-}
-
-
-
 Object.prototype.addClick = function(func){
   if (window.Touch){
     this.addEventListener('touchstart', function(e){
---
+++ public/javascripts/dataset.js
@@ -0,0 +1,17 @@
+window.currencies = {
+  "AUD": {
+    "name": "Australian Dollars",
+    "symbol": "$",
+    "rate_usd": "0.904615"
+  },
+  "JPY": {
+    "name": "Japanese Yen",
+    "symbol": "¥",
+    "rate_usd": "0.0117311"
+  },
+  "CHF": {
+    "name": "Swiss Francs",
+    "symbol": "&#8366;",
+    "rate_usd": "0.961658"
+  }
+}
