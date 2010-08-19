5c6cce756e89467b7271612dc4af1768dcb339dd
Anthony Kolber <ae@aestheticallyloyal.com>
2010-08-19 10:35:09 +1000

    Store dataset in localStorage

--- public/javascripts/dataset.js
+++ public/javascripts/dataset.js
@@ -1,17 +1,27 @@
-window.currencies = {
-  "AUD": {
-    "name": "Australian Dollars",
-    "symbol": "$",
-    "rate_usd": "0.904615"
-  },
-  "JPY": {
-    "name": "Japanese Yen",
-    "symbol": "¥",
-    "rate_usd": "0.0117311"
-  },
-  "CHF": {
-    "name": "Swiss Francs",
-    "symbol": "&#8366;",
-    "rate_usd": "0.961658"
-  }
+if(!localStorage.currencies) {
+  localStorage.currencies = JSON.stringify({
+    "AUD": {
+      "name": "Australian Dollars",
+      "symbol": "$",
+      "rate_usd": "0"
+    },
+    "JPY": {
+      "name": "Japanese Yen",
+      "symbol": "¥",
+      "rate_usd": "0"
+    },
+    "CHF": {
+      "name": "Swiss Francs",
+      "symbol": "&#8366;",
+      "rate_usd": "0"
+    },
+    "SGD" : {
+      "name": "Singapore Dollars",
+      "symbol": "S$",
+      "rate_usd": "0"
+    }
+  });
 }
+
+
+window.currencies = JSON.parse(localStorage.currencies);
