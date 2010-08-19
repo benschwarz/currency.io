394908193fa5dba60446960dce881b9b16a5d837
Anthony Kolber <ae@aestheticallyloyal.com>
2010-08-19 14:04:52 +1000

    Only create localStorage.currencies when we need it

--- public/javascripts/dataset.js
+++ public/javascripts/dataset.js
@@ -1,35 +1,37 @@
-localStorage.currencies = JSON.stringify({
-  "JPY": {
-    "name": "Japanese Yen",
-    "symbol": "¥",
-    "rate_usd": "1"
-  },
-  "AUD": {
-    "name": "Australian Dollars",
-    "symbol": "$",
-    "rate_usd": "1"
-  },
-  "CHF": {
-    "name": "Swiss Francs",
-    "symbol": "&#8366;",
-    "rate_usd": "1"
-  },
-  "SGD" : {
-    "name": "Singapore Dollars",
-    "symbol": "$",
-    "rate_usd": "1"
-  },
-  "EUR": {
-    "name": "Euro",
-    "symbol": "€",
-    "rate_usd": "1"
-  },
-  "USD": {
-    "name": "US Dollars",
-    "symbol": "$",
-    "rate_usd": "1"
-  }
-});
+if(!localStorage.currencies) {
+  localStorage.currencies = JSON.stringify({
+    "JPY": {
+      "name": "Japanese Yen",
+      "symbol": "¥",
+      "rate_usd": "1"
+    },
+    "AUD": {
+      "name": "Australian Dollars",
+      "symbol": "$",
+      "rate_usd": "1"
+    },
+    "CHF": {
+      "name": "Swiss Francs",
+      "symbol": "&#8366;",
+      "rate_usd": "1"
+    },
+    "SGD" : {
+      "name": "Singapore Dollars",
+      "symbol": "$",
+      "rate_usd": "1"
+    },
+    "EUR": {
+      "name": "Euro",
+      "symbol": "€",
+      "rate_usd": "1"
+    },
+    "USD": {
+      "name": "US Dollars",
+      "symbol": "$",
+      "rate_usd": "1"
+    }
+  });
+}
 
 if(!localStorage.from_to) {
   localStorage.from_to = JSON.stringify({ "from": "JPY", "to": "AUD" });
