6a2754cd4e44e278d5da7aef5b6549d26b60c6f2
Anthony Kolber <ae@aestheticallyloyal.com>
2010-10-06 17:28:50 +1100

    Whitespace cleanup

--- app.rb
+++ app.rb
@@ -11,15 +11,15 @@ end
 
 post '/exchange' do
   content_type :json
-  
+
   currencies = {}
   params[:currencies].split(',').each { |currency|
     request= RestClient.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'http%3A%2F%2Fwww.google.com%2Fig%2Fcalculator%3Fq%3D#{currency}%2520USD'&format=json").to_s
-    
+
     currencies[currency] = JSON.parse(request)['query']['results']['json']['rhs'].to_f
   }
-  
-  JSON.generate currencies 
+
+  JSON.generate currencies
 end
 
 #get '/offline.manifest' do
