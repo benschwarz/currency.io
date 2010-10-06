6a6dc61449b65e7e25d7aab5195369a998c41fd4
Anthony Kolber <ae@aestheticallyloyal.com>
2010-10-06 17:28:11 +1100

    Refactor currencies JSON setup

--- public/javascripts/dataset.js
+++ public/javascripts/dataset.js
@@ -1,41 +1,37 @@
-if(!localStorage.currencies) {
-  localStorage.currencies = JSON.stringify({
-    "JPY": {
-      "name": "Japanese Yen",
-      "symbol": "¥",
-      "rate_usd": "1"
-    },
-    "AUD": {
-      "name": "Australian Dollars",
-      "symbol": "$",
-      "rate_usd": "1"
-    },
-    "CHF": {
-      "name": "Swiss Francs",
-      "symbol": "&#8366;",
-      "rate_usd": "1"
-    },
-    "SGD" : {
-      "name": "Singapore Dollars",
-      "symbol": "$",
-      "rate_usd": "1"
-    },
-    "EUR": {
-      "name": "Euro",
-      "symbol": "€",
-      "rate_usd": "1"
-    },
-    "USD": {
-      "name": "US Dollars",
-      "symbol": "$",
-      "rate_usd": "1"
-    }
-  });
-}
+localStorage.currencies = localStorage.currencies || JSON.stringify({
+  "JPY": {
+    "name": "Japanese Yen",
+    "symbol": "¥",
+    "rate_usd": "1"
+  },
+  "AUD": {
+    "name": "Australian Dollars",
+    "symbol": "$",
+    "rate_usd": "1"
+  },
+  "CHF": {
+    "name": "Swiss Francs",
+    "symbol": "&#8366;",
+    "rate_usd": "1"
+  },
+  "SGD" : {
+    "name": "Singapore Dollars",
+    "symbol": "$",
+    "rate_usd": "1"
+  },
+  "EUR": {
+    "name": "Euro",
+    "symbol": "€",
+    "rate_usd": "1"
+  },
+  "USD": {
+    "name": "US Dollars",
+    "symbol": "$",
+    "rate_usd": "1"
+  }
+});
 
-if(!localStorage.from_to) {
-  localStorage.from_to = JSON.stringify({ "from": "JPY", "to": "AUD" });
-}
+localStorage.from_to = localStorage.from_to || JSON.stringify({ from: "JPY", to: "AUD" });
 
 window.from_to = JSON.parse(localStorage.from_to);
 window.currencies = JSON.parse(localStorage.currencies);
