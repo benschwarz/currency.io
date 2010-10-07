8cb2353a1a1f0701cfed01a08ee02ae8a992fa8a
Anthony Kolber <ae@aestheticallyloyal.com>
2010-10-07 22:50:14 +1100

    Refactor dataset It will now update a users localStorage if changes are made to the dataset on the server.

--- public/javascripts/dataset.js
+++ public/javascripts/dataset.js
@@ -1,52 +1,19 @@
-localStorage.currencies = localStorage.currencies || JSON.stringify({
-  "EUR": {
-    "name": "Euro",
-    "symbol": "€",
-    "rate_usd": "0"
-  },
-  "GBP": {
-    "name": "British Pound",
-    "symbol": "£",
-    "rate_usd": "0"
-  },
-  "JPY": {
-    "name": "Japanese Yen",
-    "symbol": "¥",
-    "rate_usd": "0"
-  },
-  "CHF": {
-    "name": "Swiss Francs",
-    "symbol": "chf",
-    "rate_usd": "0"
-  },
-  "CAD": {
-    "name": "Canadian Dollars",
-    "symbol": "$",
-    "rate_usd": "0"
-  },
-  "AUD": {
-    "name": "Australian Dollars",
-    "symbol": "$",
-    "rate_usd": "0"
-  },
-  "SGD": {
-    "name": "Singapore Dollars",
-    "symbol": "$",
-    "rate_usd": "0"
-  },
-  "HKD": {
-    "name": "Hong Kong Dollars",
-    "symbol": "$",
-    "rate_usd": "0"
-  },
-  "NZD": {
-    "name": "New Zealand Dollars",
-    "symbol": "$",
-    "rate_usd": "0"
-  }
+var currencies = JSON.stringify({
+  EUR: { name: "Euro", symbol: "€", rate_usd: 1 },
+  GBP: { name: "British Pound", symbol: "£", rate_usd: 1 },
+  JPY: { name: "Japanese Yen", symbol: "¥", rate_usd: 1 },
+  CHF: { name: "Swiss Francs", symbol: "chf", rate_usd: 1 },
+  CAD: { name: "Canadian Dollars", symbol: "$", rate_usd: 1 },
+  AUD: { name: "Australian Dollars", symbol: "$", rate_usd: 1 },
+  SGD: { name: "Singapore Dollars", symbol: "$", rate_usd: 1 },
+  HKD: { name: "Hong Kong Dollars", symbol: "$", rate_usd: 1 },
+  NZD: { name: "New Zealand Dollars", symbol: "$", rate_usd: 1 }
 });
 
-localStorage.from_to = localStorage.from_to || JSON.stringify({ from: "JPY", to: "AUD" });
+if (!localStorage.currencies || localStorage.currencies.length !== currencies.length) {
+  localStorage.currencies = currencies;
+  localStorage.from_to = '{ "from": "JPY", "to": "AUD" }';
+}
 
-window.from_to = JSON.parse(localStorage.from_to);
 window.currencies = JSON.parse(localStorage.currencies);
+window.from_to = JSON.parse(localStorage.from_to);
