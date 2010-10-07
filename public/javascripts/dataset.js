378d04589714d1aff32255672c91f1efccb4a93f
Anthony Kolber <ae@aestheticallyloyal.com>
2010-10-08 10:05:27 +1100

    Cleanup

--- public/javascripts/dataset.js
+++ public/javascripts/dataset.js
@@ -4,19 +4,18 @@ String.prototype.count = function(str) {
 
 var currencies = JSON.stringify({
   EUR: { name: "Euro", symbol: "€", rate_usd: 1 },
+  USD: { name: "US Dollars", symbol: "$", rate_usd: 1 },
   GBP: { name: "British Pound", symbol: "£", rate_usd: 1 },
   JPY: { name: "Japanese Yen", symbol: "¥", rate_usd: 1 },
-  USD: { name: "US Dollars", symbol: "$", rate_usd: 1 },
   CHF: { name: "Swiss Francs", symbol: "chf", rate_usd: 1 },
   CAD: { name: "Canadian Dollars", symbol: "$", rate_usd: 1 },
   AUD: { name: "Australian Dollars", symbol: "$", rate_usd: 1 },
   SGD: { name: "Singapore Dollars", symbol: "$", rate_usd: 1 },
   HKD: { name: "Hong Kong Dollars", symbol: "$", rate_usd: 1 },
-  NZD: { name: "New Zealand Dollars", symbol: "$", rate_usd: 1 }
+  NZD: { name: "New Zealand Dollars", symbol: "$", rate_usd: 1, x: "x" }
 });
 
 if (!localStorage.currencies || localStorage.currencies.count(':') !== currencies.count(':')) {
-  console.warn('refreshed currencies');
   localStorage.currencies = currencies;
   localStorage.from_to = '{ "from": "JPY", "to": "AUD" }';
 }
