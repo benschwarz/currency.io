31583cd52afd4fdc8e8050abd5a4455e77e28738
Ben Schwarz <ben.schwarz@gmail.com>
2010-12-15 15:06:32 +1100

    Use rack-client and peach to handle our HTTP requests. Yajl to handle json parsing

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
 source :rubygems
 
-gem "thin"
 gem "sinatra", "1.0"
 gem "haml"
-gem "rest-client", "1.6.0"
 gem "rack", "1.2.1"
-gem "json"
+gem "rack-client", "0.3.1.pre.f"
+gem "rack-cache"
+gem "yajl-ruby"
+gem "peach"
+
 
 gem "heroku"
--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
-require 'rest-client'
+require 'peach'
+require 'rack/client'
+require 'rack/cache'
 require 'json'
+require 'yajl'
+
+HTTPClient = Rack::Client.new do
+  use Rack::Cache,
+    :metastore   => 'heap://',
+    :entitystore => 'heap://'
+    
+  run Rack::Client::Handler::NetHTTP
+end
 
 configure do
   mime_type :manifest, "text/cache-manifest"
@@ -13,13 +24,13 @@ post '/exchange' do
   content_type :json
 
   currencies = {}
-  params[:currencies].split(',').each { |currency|
-    request= RestClient.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'http%3A%2F%2Fwww.google.com%2Fig%2Fcalculator%3Fq%3D#{currency}%2520USD'&format=json").to_s
+  params[:currencies].split(',').peach { |currency|
+    request = HTTPClient.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'http%3A%2F%2Fwww.google.com%2Fig%2Fcalculator%3Fq%3D#{currency}%2520USD'&format=json").body
 
-    currencies[currency] = JSON.parse(request)['query']['results']['json']['rhs'].to_f
+    currencies[currency] = ::Yajl::Parser.parse(request)['query']['results']['json']['rhs'].to_f
   }
-
-  JSON.generate currencies
+  
+  ::Yajl::Encoder.encode currencies
 end
 
 get '/offline.manifest' do
