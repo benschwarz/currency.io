97d52c9290923285ed82c133d026a076c864b9bf
Anthony Kolber <ae@aestheticallyloyal.com>
2011-03-12 12:15:14 +1100

    Load the video on demand

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,17 +1,17 @@
 $(function() {
-  var video = $('video').mediaelementplayer({
-    pluginPath: '/me/',
-    poster: '/videos/currency-demo.png',
-    videoWidth: 320,
-    videoHeight: 480
-  });
-
+  var video;
   $('#watch-video').click(function() {
     $('#video').show();
-  })
+    video = $('video').mediaelementplayer({
+      pluginPath: '/me/',
+      poster: '/videos/currency-demo.png',
+      videoWidth: 320,
+      videoHeight: 480
+    });
+  });
 
   $('#close-video').click(function() {
     video.stop();
     $('#video').fadeOut();
-  })
-})
+  });
+});
