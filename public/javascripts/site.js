a9c1d6ff7ff330828c22065aa589e57ca8cf037f
Ben Schwarz <ben.schwarz@gmail.com>
2011-03-21 09:02:06 +1100

    Using vimeo for video embed.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -2,12 +2,6 @@ $(function() {
   var video;
   $('#watch-video').click(function() {
     $('#video').show();
-    video = $('video').mediaelementplayer({
-      pluginPath: '/me/',
-      poster: '/videos/currency-demo.png',
-      videoWidth: 320,
-      videoHeight: 480
-    });
   });
 
   $('#close-video').click(function() {
